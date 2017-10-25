d6b6fed562d49829831a7e5368c8ee40d42ca5b6
vovnet <vovnet@yandex.ru>
2017-10-25 10:44:37 +0300

    Install devise

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'devise'
+
 gem 'haml'
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/dashboard', to: 'dashboard#index' 
   get '/days/:id', to: 'days#show'
