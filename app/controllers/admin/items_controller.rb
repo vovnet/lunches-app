b20ff30e4d9a9eeaa45c8ad51e91233c519825d7
vovnet <vovnet@yandex.ru>
2017-10-31 09:32:39 +0300

    Admin can be able to add new items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,10 +1,24 @@
 class Admin::ItemsController < Admin::AdminBaseController
   def new
+    @item = Menu.new
   end
 
   def create
+    @item = Menu.new(menu_params)
+
+    if @item.save
+      redirect_to action: "index"
+    else
+      redirect_to request.referrer
+    end
   end
 
   def index
+  end
+
+  protected
+
+  def menu_params
+    params.require(:menu).permit(:name, :price, :meal_type_id)
   end
 end
